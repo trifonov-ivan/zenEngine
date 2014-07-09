257a8a63e12043f369367f042081689ee7a4492b
ant1kvar <ivanT@playtika.by>
2014-07-09 20:42:40 +0300

    Initial Commit

---
+++ SMBuilder/SMBuilder.h
@@ -0,0 +1,13 @@
+//
+//  SMBuilder.h
+//  SMBuilder
+//
+//  Created by Ivan Trifonov on 09.07.14.
+//  Copyright (c) 2014 Ivan Trifonov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMBuilder : NSObject
+
+@end
---
+++ SMBuilder/SMBuilder.m
@@ -0,0 +1,13 @@
+//
+//  SMBuilder.m
+//  SMBuilder
+//
+//  Created by Ivan Trifonov on 09.07.14.
+//  Copyright (c) 2014 Ivan Trifonov. All rights reserved.
+//
+
+#import "SMBuilder.h"
+
+@implementation SMBuilder
+
+@end
